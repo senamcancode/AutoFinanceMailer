435be87fc18c54ebb7ceeb342e93f07544c69ad9
Senam <senam@Senams-MacBook-Pro.local>
2025-05-25 10:57:44 +0100

    added regex event handling

--- monitor.py
+++ monitor.py
@@ -2,11 +2,11 @@ from pathlib import Path
 
 import watchdog.events
 from watchdog.observers import Observer
-from watchdog.events import PatternMatchingEventHandler
+from watchdog.events import RegexMatchingEventHandler
 
-class MyHandler(PatternMatchingEventHandler):
+class MyHandler(RegexMatchingEventHandler):
     def __init__(self):
-        watchdog.events.PatternMatchingEventHandler.__init__(self, patterns=['*.pdf'], ignore_directories=True, case_sensitive=False)
+        watchdog.events.RegexMatchingEventHandler.__init__(self, regexes=[r'.*\/Downloads\/MonthlyStatement.*-\d{4}\-\d{2}\.pdf$'], ignore_directories=True, case_sensitive=False)
 
 
     def on_created(self, event):
@@ -20,6 +20,7 @@ class MyHandler(PatternMatchingEventHandler):
 
 def main():
     path = Path("test_folder")
+    #the above path will eventually be changed to Path.home()
     observer = Observer()
     observer.schedule(MyHandler(), path, recursive=True)
     observer.start()
